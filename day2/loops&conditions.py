61371c6c277ddefbd00962d65fd2b4dc5e1d0bba
kayd123 <mbakadiana84@gmail.com>
2025-08-15 00:16:50 +0300

    Day 2

---
+++ day2/loops&conditions.py
@@ -0,0 +1,38 @@
+#for loop → used when you know the range or are iterating over a collection.
+#while loop → used when you loop until a certain condition is met.
+#for loop
+import dataset
+
+for i in range(5):
+    print(i)
+my_list = [1,2,3,4,5]
+for item in my_list:
+    print(item)
+squares = [x**2 for x in range(10)]
+print(squares)
+for i in range(5):
+    print(i)  # 0 1 2 3 4
+for fruit in ["apple", "banana", "cherry"]:
+    print(fruit)
+#for idx, value in enumerate(dataset):
+#    print(idx, value)
+#While Loops
+count = 0
+while count < 5:
+    print(count)
+    count += 1
+    #condition
+loss = 1.0
+while loss > 0.05:
+    loss *= 0.9
+    print(loss)
+#⚠ Tip: Always update variables inside while loops to avoid infinite loops.
+#Conditions
+accuracy = float(input("Enter your accuracy: "))
+if accuracy > 0.9:
+    print("High accuracy")
+elif accuracy > 0.8:
+    print("Moderate accuracy")
+else:
+    print("Needs improvement")
+
---
+++ day2/task1.py
@@ -0,0 +1,6 @@
+#Write a program to print numbers 1–20, label odd/even.
+for x in range (1,21):
+    if x % 2 == 0:
+        print(f'{x} + is even')
+    else:
+        print(f'{x} + is odd')
